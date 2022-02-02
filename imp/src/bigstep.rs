5d9258f4102b7dff138b4b6c63cef302f02293a4
Newton Ni <nwtnni@gmail.com>
2022-02-01 23:33:50 -0500

    Remove `do_notation` dependency
    This is Rust, not Haskell :P
    
    While Rust doesn't have a full `Monad` trait (because it doesn't have
    higher-kinded types), it does essentially special-case the `Option`
    and `Result` types as the most common use-cases for monads.
    
    I think there's some work to generalize the question mark operator
    (essentially the same as monadic bind) via the [`Try` trait][try],
    but not clear on the stabilization timeline.
    
    People have come up with work-arounds for higher kinded types, but
    they're so (syntactically) ugly and convoluted that I doubt they'll
    see much adoption:
    
    - https://rustyyato.github.io/type/system,type/families/2021/02/15/Type-Families-1.html
    - https://hugopeters.me/posts/14/ (https://news.ycombinator.com/item?id=29508790)
    - https://www.reddit.com/r/rust/comments/6seaf8/why_are_higher_kinded_types_requested_in_rust/
    
    [try]: https://doc.rust-lang.org/std/ops/trait.Try.html

--- imp/src/bigstep.rs
+++ imp/src/bigstep.rs
@@ -1,5 +1,4 @@
 use crate::syntax::{Aexpr, Aop, Bexpr, Bop, Cmd, Cop};
-use do_notation::m;
 use std::collections::HashMap;
 use std::result::Result;
 
@@ -31,51 +30,57 @@ fn aeval(s: &Store, e: &Aexpr) -> Result<i32, String> {
     match e {
         Aexpr::Int(z) => Result::Ok(*z),
         Aexpr::Var(x) => s.get(x).copied().ok_or("Unbound variable".into()),
-        Aexpr::Op(o, e1, e2) => m! {
-            z1 <- aeval(s,e1);
-            z2 <- aeval(s,e2);
-            Result::Ok(aop_eval(*o,z1,z2))
-        },
+        Aexpr::Op(o, e1, e2) => {
+            let z1 = aeval(s, e1)?;
+            let z2 = aeval(s, e2)?;
+            Result::Ok(aop_eval(*o, z1, z2))
+        }
     }
 }
 
 fn beval(s: &Store, e: &Bexpr) -> Result<bool, String> {
     match e {
         Bexpr::Bool(b) => Result::Ok(*b),
-        Bexpr::Cop(o, e1, e2) => m! {
-            z1 <- aeval(s,e1);
-            z2 <- aeval(s,e2);
-            Result::Ok(cop_eval(*o,z1,z2))
-        },
-        Bexpr::Bop(o, e1, e2) => m! {
-            b1 <- beval(s,e1);
-            b2 <- beval(s,e2);
-            Result::Ok(bop_eval(*o,b1,b2))
-        },
+        Bexpr::Cop(o, e1, e2) => {
+            let z1 = aeval(s, e1)?;
+            let z2 = aeval(s, e2)?;
+            Result::Ok(cop_eval(*o, z1, z2))
+        }
+        Bexpr::Bop(o, e1, e2) => {
+            let b1 = beval(s, e1)?;
+            let b2 = beval(s, e2)?;
+            Result::Ok(bop_eval(*o, b1, b2))
+        }
     }
 }
 
 fn eval(s: &mut Store, c: &Cmd) -> Result<(), String> {
     match c {
         Cmd::Skip => Result::Ok(()),
-        Cmd::Ass(x, e) => m! {
-            z <- aeval(s,e);
-            Result::Ok(drop(s.insert(x.clone(),z)))
-        },
-        Cmd::Seq(c1, c2) => m! {
-            _ <- eval(s,c1); eval(s,c2)
-        },
-        Cmd::If(e, c1, c2) => m! {
-            b <- beval(s,e);
-            if b { eval(s,c1) } else { eval(s,c2) }
-        },
-        Cmd::While(e, w) => m! {
-            b <- beval(s,e);
+        Cmd::Ass(x, e) => {
+            let z = aeval(s, e)?;
+            Result::Ok(drop(s.insert(x.clone(), z)))
+        }
+        Cmd::Seq(c1, c2) => {
+            let _ = eval(s, c1)?;
+            eval(s, c2)
+        }
+        Cmd::If(e, c1, c2) => {
+            let b = beval(s, e)?;
             if b {
-            m! { _ <- eval(s,w); eval(s,c) }
+                eval(s, c1)
             } else {
-            Result::Ok(())
+                eval(s, c2)
             }
-        },
+        }
+        Cmd::While(e, w) => {
+            let b = beval(s, e)?;
+            if b {
+                let _ = eval(s, w)?;
+                eval(s, c)
+            } else {
+                Result::Ok(())
+            }
+        }
     }
 }
