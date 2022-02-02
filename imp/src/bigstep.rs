8a7cf9c451100c778073718f59ae7c7b6278fae2
Newton Ni <nwtnni@gmail.com>
2022-02-01 23:56:54 -0500

    Convert free functions to methods
    This is more of a stylistic decision. In my opinion, this is the more
    idiomatic way to write these functions: we don't have to namespace
    with `aeval`, `beval`, etc., and methods can still be called as
    functions if necessary through explicit namespacing:
    
    ```
    aop.eval(z1, z2) == Aop::eval(&aop, z1, z2)
    ```
    
    There is also some ergonomic benefit due to [deref coercion][dc].
    In practice, what this means is that methods are more lenient about
    the type of their first argument:
    
    ```
    let aop: Aop = Aop::Add;
    let aop_ref: &Aop = &Aop::Add;
    
    aop.eval(z1, z2) // OK
    aop_ref.eval(z1, z2) // OK
    
    Aop::eval(aop, z1, z2) // TYPE ERROR
    Aop::eval(&aop, z1, z2) // OK
    Aop::eval(aop_ref, z1, z2) // OK
    ```
    
    [dc]: https://doc.rust-lang.org/std/ops/trait.Deref.html#more-on-deref-coercion

--- imp/src/bigstep.rs
+++ imp/src/bigstep.rs
@@ -18,82 +18,94 @@ impl Store {
     }
 }
 
-fn aop_eval(o: Aop, z1: i32, z2: i32) -> i32 {
-    match o {
-        Aop::Add => z1 + z2,
-        Aop::Sub => z1 - z2,
-        Aop::Mul => z1 * z2,
-    }
-}
-
-fn cop_eval(o: Cop, z1: i32, z2: i32) -> bool {
-    match o {
-        Cop::Eq => z1 == z2,
-        Cop::Lt => z1 < z2,
-    }
-}
-
-fn bop_eval(o: Bop, b1: bool, b2: bool) -> bool {
-    match o {
-        Bop::And => b1 && b2,
-        Bop::Or => b1 || b2,
-    }
-}
-
-fn aeval(s: &Store, e: &Aexpr) -> Result<i32, String> {
-    match e {
-        Aexpr::Int(z) => Result::Ok(*z),
-        Aexpr::Var(x) => s.get(x),
-        Aexpr::Op(o, e1, e2) => {
-            let z1 = aeval(s, e1)?;
-            let z2 = aeval(s, e2)?;
-            Result::Ok(aop_eval(*o, z1, z2))
+impl Aop {
+    fn eval(&self, z1: i32, z2: i32) -> i32 {
+        match self {
+            Aop::Add => z1 + z2,
+            Aop::Sub => z1 - z2,
+            Aop::Mul => z1 * z2,
         }
     }
 }
 
-fn beval(s: &Store, e: &Bexpr) -> Result<bool, String> {
-    match e {
-        Bexpr::Bool(b) => Result::Ok(*b),
-        Bexpr::Cop(o, e1, e2) => {
-            let z1 = aeval(s, e1)?;
-            let z2 = aeval(s, e2)?;
-            Result::Ok(cop_eval(*o, z1, z2))
-        }
-        Bexpr::Bop(o, e1, e2) => {
-            let b1 = beval(s, e1)?;
-            let b2 = beval(s, e2)?;
-            Result::Ok(bop_eval(*o, b1, b2))
+impl Cop {
+    fn eval(&self, z1: i32, z2: i32) -> bool {
+        match self {
+            Cop::Eq => z1 == z2,
+            Cop::Lt => z1 < z2,
         }
     }
 }
 
-fn eval(s: &mut Store, c: &Cmd) -> Result<(), String> {
-    match c {
-        Cmd::Skip => Result::Ok(()),
-        Cmd::Ass(x, e) => {
-            let z = aeval(s, e)?;
-            Result::Ok(s.insert(x, z))
+impl Bop {
+    fn eval(&self, b1: bool, b2: bool) -> bool {
+        match self {
+            Bop::And => b1 && b2,
+            Bop::Or => b1 || b2,
         }
-        Cmd::Seq(c1, c2) => {
-            let _ = eval(s, c1)?;
-            eval(s, c2)
-        }
-        Cmd::If(e, c1, c2) => {
-            let b = beval(s, e)?;
-            if b {
-                eval(s, c1)
-            } else {
-                eval(s, c2)
+    }
+}
+
+impl Aexpr {
+    fn eval(&self, s: &Store) -> Result<i32, String> {
+        match self {
+            Aexpr::Int(z) => Result::Ok(*z),
+            Aexpr::Var(x) => s.get(x),
+            Aexpr::Op(o, e1, e2) => {
+                let z1 = e1.eval(s)?;
+                let z2 = e2.eval(s)?;
+                Result::Ok(o.eval(z1, z2))
             }
         }
-        Cmd::While(e, w) => {
-            let b = beval(s, e)?;
-            if b {
-                let _ = eval(s, w)?;
-                eval(s, c)
-            } else {
-                Result::Ok(())
+    }
+}
+
+impl Bexpr {
+    fn eval(&self, s: &Store) -> Result<bool, String> {
+        match self {
+            Bexpr::Bool(b) => Result::Ok(*b),
+            Bexpr::Cop(o, e1, e2) => {
+                let z1 = e1.eval(s)?;
+                let z2 = e2.eval(s)?;
+                Result::Ok(o.eval(z1, z2))
+            }
+            Bexpr::Bop(o, e1, e2) => {
+                let b1 = e1.eval(s)?;
+                let b2 = e2.eval(s)?;
+                Result::Ok(o.eval(b1, b2))
+            }
+        }
+    }
+}
+
+impl Cmd {
+    fn eval(&self, s: &mut Store) -> Result<(), String> {
+        match self {
+            Cmd::Skip => Result::Ok(()),
+            Cmd::Ass(x, e) => {
+                let z = e.eval(s)?;
+                Result::Ok(s.insert(x, z))
+            }
+            Cmd::Seq(c1, c2) => {
+                let _ = c1.eval(s)?;
+                c2.eval(s)
+            }
+            Cmd::If(e, c1, c2) => {
+                let b = e.eval(s)?;
+                if b {
+                    c1.eval(s)
+                } else {
+                    c2.eval(s)
+                }
+            }
+            Cmd::While(e, w) => {
+                let b = e.eval(s)?;
+                if b {
+                    let _ = w.eval(s)?;
+                    self.eval(s)
+                } else {
+                    Result::Ok(())
+                }
             }
         }
     }
