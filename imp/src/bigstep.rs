f2b666fd2c74a2e640a18a8d158cc41a9f1f823d
Newton Ni <nwtnni@gmail.com>
2022-02-01 23:19:23 -0500

    Remove redundant prefixes
    I'm guessing this habit is from OCaml, where you need to have some
    namespacing in the variant name to avoid collisions. I think Rust made
    the better choice to scope variants under the type name, so that by
    default you'll write, for example, `Aexpr::Int` instead of `Int`, and
    therefore there's no need to use `AInt`.
    
    If the fully qualified name is too verbose, it's common to import the
    variant names locally:
    
    ```rust
    fn scoped_import(expr: Aexpr) {
        use Aexpr::*;
        match expr {
            Int(_) => (),
            Var(_) => (),
        }
    }
    ```

--- imp/src/bigstep.rs
+++ imp/src/bigstep.rs
@@ -15,31 +15,31 @@ fn ignore<A>(_: A) -> () {
 
 fn aop_eval(o: Aop, z1: i32, z2: i32) -> i32 {
     match o {
-        Aop::AAdd => z1 + z2,
-        Aop::ASub => z1 - z2,
-        Aop::AMul => z1 * z2,
+        Aop::Add => z1 + z2,
+        Aop::Sub => z1 - z2,
+        Aop::Mul => z1 * z2,
     }
 }
 
 fn cop_eval(o: Cop, z1: i32, z2: i32) -> bool {
     match o {
-        Cop::CEq => z1 == z2,
-        Cop::CLt => z1 < z2,
+        Cop::Eq => z1 == z2,
+        Cop::Lt => z1 < z2,
     }
 }
 
 fn bop_eval(o: Bop, b1: bool, b2: bool) -> bool {
     match o {
-        Bop::BAnd => b1 && b2,
-        Bop::BOr => b1 || b2,
+        Bop::And => b1 && b2,
+        Bop::Or => b1 || b2,
     }
 }
 
 fn aeval(s: &Store, e: &Aexpr) -> Result<i32, String> {
     match e {
-        Aexpr::AInt(z) => Result::Ok(*z),
-        Aexpr::AVar(x) => deref_option(s.get(x)).ok_or("Unbound variable".into()),
-        Aexpr::AOp(o, e1, e2) => m! {
+        Aexpr::Int(z) => Result::Ok(*z),
+        Aexpr::Var(x) => deref_option(s.get(x)).ok_or("Unbound variable".into()),
+        Aexpr::Op(o, e1, e2) => m! {
             z1 <- aeval(s,e1);
             z2 <- aeval(s,e2);
             Result::Ok(aop_eval(*o,z1,z2))
@@ -49,13 +49,13 @@ fn aeval(s: &Store, e: &Aexpr) -> Result<i32, String> {
 
 fn beval(s: &Store, e: &Bexpr) -> Result<bool, String> {
     match e {
-        Bexpr::BBool(b) => Result::Ok(*b),
-        Bexpr::BCop(o, e1, e2) => m! {
+        Bexpr::Bool(b) => Result::Ok(*b),
+        Bexpr::Cop(o, e1, e2) => m! {
             z1 <- aeval(s,e1);
             z2 <- aeval(s,e2);
             Result::Ok(cop_eval(*o,z1,z2))
         },
-        Bexpr::BBop(o, e1, e2) => m! {
+        Bexpr::Bop(o, e1, e2) => m! {
             b1 <- beval(s,e1);
             b2 <- beval(s,e2);
             Result::Ok(bop_eval(*o,b1,b2))
@@ -65,19 +65,19 @@ fn beval(s: &Store, e: &Bexpr) -> Result<bool, String> {
 
 fn eval(s: &mut Store, c: &Cmd) -> Result<(), String> {
     match c {
-        Cmd::CSkip => Result::Ok(()),
-        Cmd::CAss(x, e) => m! {
+        Cmd::Skip => Result::Ok(()),
+        Cmd::Ass(x, e) => m! {
             z <- aeval(s,e);
             Result::Ok(ignore(s.insert(x.clone(),z)))
         },
-        Cmd::CSeq(c1, c2) => m! {
+        Cmd::Seq(c1, c2) => m! {
             _ <- eval(s,c1); eval(s,c2)
         },
-        Cmd::CIf(e, c1, c2) => m! {
+        Cmd::If(e, c1, c2) => m! {
             b <- beval(s,e);
             if b { eval(s,c1) } else { eval(s,c2) }
         },
-        Cmd::CWhile(e, w) => m! {
+        Cmd::While(e, w) => m! {
             b <- beval(s,e);
             if b {
             m! { _ <- eval(s,w); eval(s,c) }
--- imp/src/syntax.rs
+++ imp/src/syntax.rs
@@ -1,44 +1,44 @@
 // Arithmetic operators.
 #[derive(Clone, Copy)]
 pub enum Aop {
-    AAdd,
-    ASub,
-    AMul,
+    Add,
+    Sub,
+    Mul,
 }
 
 // Arithmetic expressions.
 pub enum Aexpr {
-    AInt(i32),
-    AVar(String),
-    AOp(Aop, Box<Aexpr>, Box<Aexpr>),
+    Int(i32),
+    Var(String),
+    Op(Aop, Box<Aexpr>, Box<Aexpr>),
 }
 
 // Comparison operators.
 #[derive(Clone, Copy)]
 pub enum Cop {
-    CEq,
-    CLt,
+    Eq,
+    Lt,
 }
 
 // Boolean operators.
 #[derive(Clone, Copy)]
 pub enum Bop {
-    BAnd,
-    BOr,
+    And,
+    Or,
 }
 
 // Boolean expressions.
 pub enum Bexpr {
-    BBool(bool),
-    BCop(Cop, Box<Aexpr>, Box<Aexpr>),
-    BBop(Bop, Box<Bexpr>, Box<Bexpr>),
+    Bool(bool),
+    Cop(Cop, Box<Aexpr>, Box<Aexpr>),
+    Bop(Bop, Box<Bexpr>, Box<Bexpr>),
 }
 
 // Commands.
 pub enum Cmd {
-    CSkip,
-    CAss(String, Box<Aexpr>),
-    CSeq(Box<Cmd>, Box<Cmd>),
-    CIf(Box<Bexpr>, Box<Cmd>, Box<Cmd>),
-    CWhile(Box<Bexpr>, Box<Cmd>),
+    Skip,
+    Ass(String, Box<Aexpr>),
+    Seq(Box<Cmd>, Box<Cmd>),
+    If(Box<Bexpr>, Box<Cmd>, Box<Cmd>),
+    While(Box<Bexpr>, Box<Cmd>),
 }
