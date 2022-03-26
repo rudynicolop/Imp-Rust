41381cc4451996bea9127c39602bf987d1c080a5
rnp39 <rnp39@cornell.edu>
2022-03-25 18:46:14 -0700

    wish lexers in rust were not sad...

--- imp/src/lexer.rs
+++ imp/src/lexer.rs
@@ -53,33 +53,117 @@ impl fmt::Display for Token {
     }
 }
 
+// The following is stolen from the [LALRPOP] tutorial.
+
+pub type Spanned<Tok, Loc, Error> = Result<(Loc, Tok, Loc), Error>;
+
 // Lexical errors.
 pub enum Error {
     NoToken(String)
 }
 
-// The following is stolen from the [LALRPOP] tutorial.
-
 pub struct Lexer<'input> {
-    chars : CharIndices<'input>
+    chars : CharIndices<'input>; // token stream
+    // next : Option(usize,char) // next character
 }
 
 impl<'input> Lexer<'input> {
     pub fn new(input: &'input str) -> Self {
-        Lexer { chars: input.char_indices() }
+	let stream := input.char_indices();
+        Lexer { chars: input.char_indices();
+		//next: stream.next()
+	}
+    }
+
+    /*
+    pub fn peek(&self) -> Option<usize,char> {
+	self.next()
+    }
+     */
+
+    fn match_token_helper(&self,pair : (usize,char)) -> Option(usize,Token,usize) {
+	let i = pair.0;
+	match pair.1 {
+	    '{' => Some ((i,Token::LBRACE,i+1)),
+	    '}' => Some ((i,Token::RBRACE,i+1)),
+	    '(' => Some ((i,Token::LPAREN,i+1)),
+	    ')' => Some ((i,Token::RPAREN,i+1)),
+	    ';' => Some ((i,Token::SEMICOLON,i+1)),
+	    '+' => Some ((i,Token::ADD,i+1)),
+	    '*' => Some ((i,Token::MUL,i+1)),
+	    '-' => Some ((i,Token::SUB,i+1)),
+	    ':' =>
+		match self.chars().next() {
+		    Some ((_,'=')) => Some ((i,Token::ASGN,i+2)),
+		    None           => None },
+	    '=' =>
+		match self.chars().next() {
+		    Some ((_,'?')) => Some ((i,Token::EQ,i+2)),
+		    None           => None },
+	    '<' =>
+		match self.chars().next() {
+		    Some ((_,'?')) => Some ((i,Token::LT,i+2)),
+		    None           => None },
+	    'i' =>
+		match self.chars().next() {
+		    Some ((_,'f')) => Some ((i,Token::IF,i+2)),
+		    None           => None },
+	    'o' =>
+		match self.chars().next() {
+		    Some ((_,'r')) => Some ((i,Token::OR,i+2)),
+		    None           => None },
+	    'a' =>
+		match self.chars().next() {
+		    Some ((_,'n')) =>
+			match chars().next() {}
+			Some ((i,Token::IF,i+2)),
+		    None           => None },
+	    
+	}
+    }
+    
+    /// Returns an optional end index and symbol token.
+    /// Advances the cursor.
+    pub fn match_token(&self) -> Option<(usize,Token,usize)> {
+	self.chars().next().and_then
+	    Some((i,'{')) => Some ((i,Token::LBRACE,i+1)),
+	    Some((i,'}')) => Some ((i,Token::RBRACE,i+1)),
+	    Some((i,'(')) => Some ((i,Token::LPAREN,i+1)),
+	    Some((i,')')) => Some ((i,Token::RPAREN,i+1)),
+	    Some((i,';')) => Some ((i,Token::SEMICOLON,i+1)),
+	    Some((i,'+')) => Some ((i,Token::ADD,i+1)),
+	    Some((i,'*')) => Some ((i,Token::MUL,i+1)),
+	    Some((i,'-')) => Some ((i,Token::SUB,i+1)),
+	Some((i,':')) =>
+	    match self.chars().next() {
+		    Some ((_,'=')) => Some ((i,Token::ASGN,i+2)),
+		    None           => None
+		}
+	}
     }
 }
 
-/*
 impl<'a> Iterator for Lexer<'a> {
     type Item = Spanned<Token, usize, Error>;
     
     fn next(&mut self) -> Option<Self::Item> {
 	loop {
-	    match self.chars.next() {
-		None => None, // EOF
-		Some((i,))
+	    match self.chars.peek() {
+		None          => return None, // EOF
+		Some((_, ' ')) |
+		Some((_, '\n')) |
+		Some((_, '\t')) => continue, // whitespace
+		Some((i,'{')) => Some ((i,Token::LBRACE,i+1)),
+		Some((i,'}')) => Some ((i,Token::RBRACE,i+1)),
+		Some((i,'(')) => Some ((i,Token::LPAREN,i+1)),
+		Some((i,')')) => Some ((i,Token::RPAREN,i+1)),
+		Some((i,';')) => Some ((i,Token::SEMICOLON,i+1)),
+		Some((i,'+')) => Some ((i,Token::ADD,i+1)),
+		Some((i,'*')) => Some ((i,Token::MUL,i+1)),
+		Some((i,'-')) => Some ((i,Token::SUB,i+1)),
+		Some()
+		_ => continue
 	    }
 	}
     }
-} */
+}
